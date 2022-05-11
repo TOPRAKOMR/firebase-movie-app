109f92db7f716e390dad043fad21ddffa1251c45
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-05-12 01:08:10 +0300

    update

--- solution/src/router/AppRouter.js
+++ solution/src/router/AppRouter.js
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+
 
 
 const AppRouter = () => {
@@ -13,6 +15,7 @@ const AppRouter = () => {
         <Route path="/" element={<Main/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/details/:id" element={<MovieDetail/>}/>
       </Routes>
 
     </Router>
